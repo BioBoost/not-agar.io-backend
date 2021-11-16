56b8bce72f173708c32cfbf3df5f2a9e1b9c1e2e
Nico De Witte <nico.dewitte@outlook.com>
2021-11-16 20:22:37 +0100

    Allow image to be send to display

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,14 @@ dotenv.config();
 
 const config = {
   general: {
-    port: process.env.PORT || 3000
+    port: process.env.PORT || 3000,
+  },
+  frontend: {
+    port: process.env.FRONTEND_PORT || 8080
+  },
+  display: {
+    enabled: process.env.ENABLE_DISPLAY || false,
+    host: process.env.DISPLAY_HOST || 'http://10.0.0.99:3000'
   }
 }
 
--- src/server.js
+++ src/server.js
@@ -1,15 +1,62 @@
 import config from './config/config.js'
 import express from 'express';
+import cors from 'cors';
+import SocketIO from 'socket.io';
+import axios from 'axios';
 
 const app = express();
+const server = require('http').createServer(app);
+
+app.use(cors({
+  origin: `http://localhost:${config.frontend.port}`,
+}));
+app.use(express.json({limit: '1mb'}));    // Allow bigger body payloads
+
+const io = SocketIO(server, {
+  cors: {
+    origin: `http://localhost:${config.frontend.port}`,
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on('connection', function (socket) {
+  console.log('Phaser frontend connected');
+
+  socket.on('disconnect', function () {
+    console.log('Phaser frontend disconnected');
+  });
+});
 
 app.get('/', (req, res) => {
   res.send({
     message: 'Welcome to not-agar.io backend API',
     version: process.env.npm_package_version
   })
-})
+});
 
-app.listen(config.general.port, () => {
+app.post('/display', (req, res) => {
+  if (!config.display.enabled) {
+    return res.send({
+      message: 'Image received but display is not enabled'
+    });
+  }
+
+  let buffer = Buffer.from(req.body.data, 'base64');    // Convert base64 data into raw png again
+  axios.post(`${config.display.host}`, buffer)
+  .then(function () {
+    res.send({
+      message: 'Image successfully send to display'
+    });
+  })
+  .catch(function (error) {
+    res.status(500).send({
+      message: 'Failed to send image to display',
+      error: error
+    })
+  });
+
+});
+
+server.listen(config.general.port, () => {
   console.log(`Starting not-agar.io backend API on http://localhost:${config.general.port}`);
 });
