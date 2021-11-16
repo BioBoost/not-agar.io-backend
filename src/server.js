e4f73c050e1cff66283001ab3c50e22d74493221
Nico De Witte <nico.dewitte@outlook.com>
2021-11-16 20:08:23 +0100

    Add welcome route

--- src/server.js
+++ src/server.js
@@ -3,6 +3,13 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send({
+    message: 'Welcome to not-agar.io backend API',
+    version: process.env.npm_package_version
+  })
+})
+
 app.listen(config.general.port, () => {
   console.log(`Starting not-agar.io backend API on http://localhost:${config.general.port}`);
 });
